30daa71371a0762a7edc9b79b3865dc639305a30
Fracchiolla <raffaelefracchiolla05@gmail.com>
2024-03-27 09:04:46 +0100

    manage Fracchiolla case

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,9 +7,12 @@ for num in range(x-25,x+25):
      print("Buzz",end="")
  if num % 14 == 0:
      print("AprilFish",end="")
+ if num % 143 == 0:
+     print("FracchiollaRaffaele",end="")
  if num % 3 != 0 \
     and num % 5 != 0 \
-    and num % 14 != 0:
+    and num % 14 != 0 \
+    and num % 143 != 0:
      print(num,end="")
  print("")
      
